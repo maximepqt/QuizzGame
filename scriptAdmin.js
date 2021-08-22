64fb7c57325b42d7b9ba676d8ec6846a12642195
Maxime PAQUET <maxime.paquet1245@aol.fr>
2021-08-22 16:32:45 +0200

    Creation of question reponse working creating localstore

--- scriptAdmin.js
+++ scriptAdmin.js
@@ -9,6 +9,7 @@ document.getElementById("submit").addEventListener("click", function (e) {
    
 });
 
+
 function isNumberCorrect(nombreDeQuestion){
     if(nombreDeQuestion >=61){
         window.alert("Le max est 60");
@@ -16,32 +17,46 @@ function isNumberCorrect(nombreDeQuestion){
         nombreDeQuestion = 0;
         location.reload();
         
+    }else{
+
+        let gestionQuestion = document.getElementById("Questions");
+        let gestionReponse = document.getElementById("Reponses");
+        let gestionDelete = document.getElementById("Delete");
+        let PlayLink = document.getElementById("ClassPlayLink");
+
+        gestionQuestion.innerHTML="";
+        gestionReponse.innerHTML="";
+        gestionDelete.innerHTML="";
+
+        for (let i = 0; i < nombreDeQuestion; i++) {
+
+             gestionQuestion = document.getElementById("Questions");
+            let textQuestion = document.createElement("input");
+            textQuestion.classList.add("ClassQuestions");
+            textQuestion.type = "text";
+            textQuestion.placeholder = "Question " + (i+1);
+            gestionQuestion.appendChild(textQuestion);
+
+
+             gestionReponse = document.getElementById("Reponses");
+            let textReponse = document.createElement("input");
+            textReponse.classList.add("ClassReponses");
+            textReponse.type = "text";
+            textReponse.placeholder = "Reponse " + (i+1);
+            gestionReponse.appendChild(textReponse);
+
+            
+            }
+           
+            PlayLink.style.visibility = "visible";
+
+
+            
     }
     
 }
 
 
 
-for (let i = 0; i < nombreDeQuestion; i++) {
-    // let block = document.createElement("div");
-    // block.classList.add("question");
 
-    // let questionBlock = document.createElement("div");
-    // questionBlock.classList.add("questionBlock");
-    // block.appendChild(questionBlock);
-
-    // let questionButton = document.createElement("button");
-    // questionButton.textContent = "Question " + questions[i][0];
-    // questionButton.addEventListener("click", function (e) {
-    //     e.preventDefault();
-    //     showQuestion(i);
-    // });
-    // questionBlock.appendChild(questionButton);
-
-    let textQuestion = document.createElement("input");
-    textQuestion.type = "text";
-    appendChild(gestionQuestionReponse);
-
-    document.body.appendChild(block);
-}
 
