c9f99e07521b27cb9a99364243395ff5ebf25799
Maxime PAQUET <maxime.paquet1245@aol.fr>
2021-08-23 18:14:09 +0200

    Random on page admin for question/response done

--- scriptAdmin.js
+++ scriptAdmin.js
@@ -4,7 +4,8 @@ let nombreDeQuestion = 0;
 document.getElementById("submit").addEventListener("click", function (e) {
     e.preventDefault();
    
-    nombreDeQuestion = document.getElementById("userNumberQuestion").value;
+    nombreDeQuestion = parseInt(document.getElementById("userNumberQuestion").value);
+    nombreDeJoueur = document.getElementById("userNumberPlayer").value;
     localStorage.setItem("nombreDeQuestion",nombreDeQuestion);
     isNumberCorrect(nombreDeQuestion);
    
@@ -12,8 +13,8 @@ document.getElementById("submit").addEventListener("click", function (e) {
 
 
 function isNumberCorrect(nombreDeQuestion){
-    if(nombreDeQuestion >=61){
-        window.alert("Le max est 60");
+    if(nombreDeQuestion >=61 ||nombreDeJoueur == 0 ){
+        window.alert("Le max de questions est 60 et le min de joueur est 1");
         //console.log("Le max est 60");
         nombreDeQuestion = 0;
         location.reload();
@@ -28,7 +29,8 @@ function isNumberCorrect(nombreDeQuestion){
         gestionQuestion.innerHTML="";
         gestionReponse.innerHTML="";
         gestionDelete.innerHTML="";
-
+        let myNumberInc = nombreDeQuestion 
+        console.log(myNumberInc);
         for (let i = 0; i < nombreDeQuestion; i++) {
 
              gestionQuestion = document.getElementById("Questions");
@@ -36,6 +38,7 @@ function isNumberCorrect(nombreDeQuestion){
             textQuestion.classList.add("ClassQuestions");
             textQuestion.type = "text";
             textQuestion.placeholder = "Question " + (i+1);
+            textQuestion.id = "Question" + (i+1);
             gestionQuestion.appendChild(textQuestion);
 
 
@@ -44,10 +47,120 @@ function isNumberCorrect(nombreDeQuestion){
             textReponse.classList.add("ClassReponses");
             textReponse.type = "text";
             textReponse.placeholder = "Reponse " + (i+1);
+            textReponse.id = "Reponse" + (i+1);
             gestionReponse.appendChild(textReponse);
 
             
             }
+            
+            //Gestion de notre color randomizer
+            for (var i = 1, ar = []; i < nombreDeQuestion+1; i++) {
+                ar[i] = i;
+              }
+            
+              // randomize the array
+              ar.sort(function () {
+                  return Math.random() - 0.5;
+              });
+
+              let nombreDeCouleur =nombreDeQuestion/=nombreDeJoueur;
+              let inc =0;
+              let questionName,reponseName;
+              let chgtBG;
+              for (let i = 0;i<nombreDeJoueur;i++){
+                  for(let y = 0;y<nombreDeCouleur;y++){
+                      
+                      
+                      console.log(i,y,inc);
+
+                      if(inc<10){
+                          //Question en theme Rouge
+                          console.log("tab" + ar[inc]);
+                          questionName = "Question" + ar[inc];
+                          console.log(questionName);
+                          chgtBG = document.getElementById(questionName);
+                          chgtBG.style.backgroundColor ="red";
+                        //reponse en theme Rouge
+                        reponseName = "Reponse" + ar[inc];
+                        chgtBG = document.getElementById(reponseName);
+                        chgtBG.style.backgroundColor ="red";
+                        
+
+                        
+                      }
+
+                      if(inc >=10 && inc<20){
+                        //Question en theme Rouge
+                        console.log("tab" + ar[inc]);
+                        questionName = "Question" + ar[inc];
+                        console.log(questionName);
+                        chgtBG = document.getElementById(questionName);
+                        chgtBG.style.backgroundColor ="blue";
+                      //reponse en theme Rouge
+                      reponseName = "Reponse" + ar[inc];
+                      chgtBG = document.getElementById(reponseName);
+                      chgtBG.style.backgroundColor ="blue";
+                      
+                      }
+
+                      if(inc >=20 && inc<30){
+                        //Question en theme Rouge
+                        console.log("tab" + ar[inc]);
+                        questionName = "Question" + ar[inc];
+                        console.log(questionName);
+                        chgtBG = document.getElementById(questionName);
+                        chgtBG.style.backgroundColor ="green";
+                      //reponse en theme Rouge
+                      reponseName = "Reponse" + ar[inc];
+                      chgtBG = document.getElementById(reponseName);
+                      chgtBG.style.backgroundColor ="green";
+                      
+                      }
+
+                      if(inc >=30 && inc<40){
+                        //Question en theme Rouge
+                        console.log("tab" + ar[inc]);
+                        questionName = "Question" + ar[inc];
+                        console.log(questionName);
+                        chgtBG = document.getElementById(questionName);
+                        chgtBG.style.backgroundColor ="yellow";
+                      //reponse en theme Rouge
+                      reponseName = "Reponse" + ar[inc];
+                      chgtBG = document.getElementById(reponseName);
+                      chgtBG.style.backgroundColor ="yellow";
+                      
+                      }
+
+                      if(inc >=40 && inc<50){
+                        //Question en theme Rouge
+                        console.log("tab" + ar[inc]);
+                        questionName = "Question" + ar[inc];
+                        console.log(questionName);
+                        chgtBG = document.getElementById(questionName);
+                        chgtBG.style.backgroundColor ="purple";
+                      //reponse en theme Rouge
+                      reponseName = "Reponse" + ar[inc];
+                      chgtBG = document.getElementById(reponseName);
+                      chgtBG.style.backgroundColor ="purple";
+                      
+                      }
+
+                      if(inc >=50 && inc<60){
+                        //Question en theme Rouge
+                        console.log("tab" + ar[inc]);
+                        questionName = "Question" + ar[inc];
+                        console.log(questionName);
+                        chgtBG = document.getElementById(questionName);
+                        chgtBG.style.backgroundColor ="orange";
+                      //reponse en theme Rouge
+                      reponseName = "Reponse" + ar[inc];
+                      chgtBG = document.getElementById(reponseName);
+                      chgtBG.style.backgroundColor ="orange";
+                      
+                      }
+                      inc +=1;
+                  }
+              }
            
             PlayLink.style.visibility = "visible";
 
